80fc607666c2cd34ce19b06bf2fa158e63a2df06
Siv Midtun Hollup <siv.hollup@gmail.com>
2020-08-05 13:38:39 +0200

    Bare bones build setup

---
+++ src/test/java/no/kantega/kata/PricingTest.java
@@ -0,0 +1,13 @@
+package no.kantega.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PricingTest {
+
+    @Test
+    void helloWorld() {
+        assertEquals("Hello world", "Hello world");
+    }
+}
